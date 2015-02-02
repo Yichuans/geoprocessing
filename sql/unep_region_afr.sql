ac358c1ecd04f59f9003439b3315d32b65130411
Yichuans <yichuan.shi@outlook.com>
2015-02-02 11:41:36 +0000

    initial commit sql

---
+++ sql/unep_region_afr.sql
@@ -0,0 +1,23 @@
+﻿WITH a AS (
+SELECT 
+  afr.patch_id, 
+  afr.year, 
+  afr.count_pixel, 
+  afr.total_area_km2, 
+  iso3_code,
+  country_ecoregion.terr_name, 
+  country_ecoregion.eco_name, 
+  country_ecoregion.biome, 
+  country_ecoregion.realm
+FROM 
+  ad_hoc.afr, 
+  ad_hoc.country_ecoregion
+WHERE 
+  afr.patch_id = country_ecoregion.patch_id
+)
+SELECT iso3_code, terr_name, year, sum(total_area_km2) as total_area_km2
+FROM a
+WHERE year != 0 AND iso3_code not in ('ESP', ' ', 'ISR', 'PSE', 'XXX')
+GROUP BY iso3_code, terr_name, year
+ORDER BY iso3_code, year
+  
