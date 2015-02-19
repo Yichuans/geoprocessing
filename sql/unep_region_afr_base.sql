c5b4e3914e2b74752c8dcf8e33099ac534818776
Yichuans <yichuan.shi@outlook.com>
2015-02-19 10:04:30 +0000

    pgsql to calculate baseline forest stats

---
+++ sql/unep_region_afr_base.sql
@@ -0,0 +1,71 @@
+﻿-- country level aggregates
+CREATE OR REPLACE VIEW ad_hoc.unep_afr_baseline_country AS
+WITH a AS (
+SELECT 
+  afr_baseline.patch_id,  
+  afr_baseline.count_pixel, 
+  afr_baseline.total_area_km2, 
+  iso3_code,
+  country_ecoregion.terr_name, 
+  country_ecoregion.eco_name, 
+  country_ecoregion.biome, 
+  country_ecoregion.realm
+FROM 
+  ad_hoc.afr_baseline, 
+  ad_hoc.country_ecoregion
+WHERE 
+  afr_baseline.patch_id = country_ecoregion.patch_id AND iso3_code not in ('ESP', ' ', 'ISR', 'PSE', 'XXX')
+)
+SELECT iso3_code, terr_name, sum(total_area_km2) ::double precision as total_area_km2
+FROM a
+GROUP BY iso3_code, terr_name
+ORDER BY iso3_code;
+  
+-- ecoregion level aggregates across continents
+CREATE OR REPLACE VIEW ad_hoc.unep_afr_baseline_ecoregion AS
+WITH a AS (
+SELECT 
+  afr_baseline.patch_id,  
+  afr_baseline.count_pixel, 
+  afr_baseline.total_area_km2, 
+  iso3_code,
+  country_ecoregion.terr_name, 
+  country_ecoregion.eco_name, 
+  country_ecoregion.biome, 
+  country_ecoregion.realm
+FROM 
+  ad_hoc.afr_baseline, 
+  ad_hoc.country_ecoregion
+WHERE 
+  afr_baseline.patch_id = country_ecoregion.patch_id AND iso3_code not in ('ESP', ' ', 'ISR', 'PSE', 'XXX')
+)
+SELECT eco_name, biome, realm, sum(total_area_km2)::double precision as total_area_km2
+FROM a
+GROUP BY eco_name, biome, realm
+ORDER BY eco_name, biome, realm;
+
+-- biome level aggregates across continents
+CREATE OR REPLACE VIEW ad_hoc.unep_afr_baseline_biome AS
+WITH a AS (
+SELECT 
+  afr_baseline.patch_id,  
+  afr_baseline.count_pixel, 
+  afr_baseline.total_area_km2, 
+  iso3_code,
+  country_ecoregion.terr_name, 
+  country_ecoregion.eco_name, 
+  country_ecoregion.biome,
+  _lookup_wwf_terr_biome.name as biome_name,
+  country_ecoregion.realm
+FROM 
+  ad_hoc.afr_baseline, 
+  ad_hoc.country_ecoregion,
+  ad_hoc._lookup_wwf_terr_biome
+WHERE 
+ bioid = biome AND
+  afr_baseline.patch_id = country_ecoregion.patch_id AND iso3_code not in ('ESP', ' ', 'ISR', 'PSE', 'XXX')
+)
+SELECT biome, biome_name, sum(total_area_km2)::double precision as total_area_km2
+FROM a
+GROUP BY biome, biome_name
+ORDER BY biome;
